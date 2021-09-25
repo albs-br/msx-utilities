5655677543fd57520d81caee0d9d2950c770788e
Andre Baptista <albs_br@yahoo.com.br>
2021-09-25 15:11:52 -0300

    v.0.39.4

--- MSXUtilities/Program.cs
+++ MSXUtilities/Program.cs
@@ -43,118 +43,12 @@ namespace MSXUtilities
 
             // Import tilemap from Tiled Map Editor
             var Tiled_TileMapFilePath = @"C:\Users\albs_\OneDrive\Desktop\MSX development\GoPenguin\tile map test..csv";
-            var tilemapConverted = ImportTileMapFromTiled(Tiled_TileMapFilePath);
+            var baseLabel = "BgObjectsInitialState_TestLevel_2";
+            var tilemapConverted = GoPenguin.TileMaps.ImportTileMapFromTiled.Execute(Tiled_TileMapFilePath, baseLabel);
             GoPenguin.TileMaps.CreateTileMap.Execute(tilemapConverted);
 
             Console.WriteLine("Done.");
             Console.ReadLine();
-        }
-
-        private static List<List<int>> ImportTileMapFromTiled(string tileMapFilePath)
-        {
-            var output = new List<List<int>>();
-            var file = File.ReadAllText(tileMapFilePath);
-
-            var startLabel = "BgObjectsInitialState_TestLevel_2_Start";
-            var bgObjectsText = startLabel + ":" + Environment.NewLine + Environment.NewLine;
-
-            var lines = file.Split(Environment.NewLine).ToList();
-            lines  = lines.Where(x => x.Trim() != "").ToList();
-            if (lines.Count() != 12) throw new InvalidDataException("Tilemap file must have exactly 12 lines");
-
-            int x = 0, y = 0;
-            var arrayBgObjectsScreens = new string[16];
-            foreach (var line in lines)
-            {
-                var outputLine = new List<int>();
-
-                var arrayLine = line.Split(",");
-                if (arrayLine.Count() != 256) throw new InvalidDataException("Each line of the tilemap file must have exactly 256 tiles");
-
-                x = 0;
-                //int lastScreen = 0;
-                foreach (var item in arrayLine)
-                {
-                    int currentScreen = x / 16;
-                    //if (currentScreen > lastScreen)
-                    //{
-                    //    bgObjectsText += String.Format(
-                    //        "\tds     256 - ($ - ({0} + {1})), 0                 ; fill with 0s until end of block",
-                    //        startLabel,
-                    //        currentScreen * 256
-                    //        );
-                    //    lastScreen = currentScreen;
-                    //}
-
-                    var intValueConverted = 0;
-                    switch (int.Parse(item))
-                    {
-                        case -1:
-                            intValueConverted = 0;
-                            break;
-
-                        case 0:
-                            // Big Bricks
-                            intValueConverted = 2;
-                            break;
-
-                        case 1:
-                            // Small Bricks
-                            intValueConverted = 1;
-                            break;
-
-                        case 2:
-                            // Diamond
-                            //intValueConverted = 0;
-                            arrayBgObjectsScreens[currentScreen] += 
-                                String.Format("\tdb      {0},      {1},     {2} * 2 * 8,      1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0" + Environment.NewLine,
-                                x,
-                                "DIAMOND_FIRST_TILE",
-                                y
-                                );
-
-                            break;
-
-                        case 3:
-                            // Grass
-                            intValueConverted = 3;
-                            break;
-
-                        case 4:
-                            // Rocks
-                            intValueConverted = 4;
-                            break;
-
-                        default:
-                            break;
-                    }
-                    outputLine.Add(intValueConverted);
-
-                    x++;
-                }
-                
-                output.Add(outputLine);
-
-                y++;
-            }
-
-            // Save BgObjects file
-            int index = 0;
-            foreach (var item in arrayBgObjectsScreens)
-            {
-                bgObjectsText += "; Screen #" + index + Environment.NewLine;
-                bgObjectsText += item;
-                bgObjectsText += String.Format(
-                    "\tds     256 - ($ - ({0} + {1})), 0                 ; fill with 0s until end of block" + Environment.NewLine + Environment.NewLine,
-                    startLabel,
-                    index * 256
-                    );
-                index++;
-            }
-            bgObjectsText += "BgObjectsInitialState_TestLevel_2_End:" + Environment.NewLine;
-            File.WriteAllText("BgObjects.s", bgObjectsText);
-
-            return output;
         }
 
         static void CreateTilesForGoPenguin()
