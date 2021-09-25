abe74bc665ff3069ab881e242fbf932b3b2320a9
Andre Baptista <albs_br@yahoo.com.br>
2021-09-24 23:23:03 -0300

    v.0.39.3

--- MSXUtilities/Program.cs
+++ MSXUtilities/Program.cs
@@ -54,11 +54,16 @@ namespace MSXUtilities
         {
             var output = new List<List<int>>();
             var file = File.ReadAllText(tileMapFilePath);
-            
+
+            var startLabel = "BgObjectsInitialState_TestLevel_2_Start";
+            var bgObjectsText = startLabel + ":" + Environment.NewLine + Environment.NewLine;
+
             var lines = file.Split(Environment.NewLine).ToList();
             lines  = lines.Where(x => x.Trim() != "").ToList();
             if (lines.Count() != 12) throw new InvalidDataException("Tilemap file must have exactly 12 lines");
 
+            int x = 0, y = 0;
+            var arrayBgObjectsScreens = new string[16];
             foreach (var line in lines)
             {
                 var outputLine = new List<int>();
@@ -66,8 +71,21 @@ namespace MSXUtilities
                 var arrayLine = line.Split(",");
                 if (arrayLine.Count() != 256) throw new InvalidDataException("Each line of the tilemap file must have exactly 256 tiles");
 
+                x = 0;
+                //int lastScreen = 0;
                 foreach (var item in arrayLine)
                 {
+                    int currentScreen = x / 16;
+                    //if (currentScreen > lastScreen)
+                    //{
+                    //    bgObjectsText += String.Format(
+                    //        "\tds     256 - ($ - ({0} + {1})), 0                 ; fill with 0s until end of block",
+                    //        startLabel,
+                    //        currentScreen * 256
+                    //        );
+                    //    lastScreen = currentScreen;
+                    //}
+
                     var intValueConverted = 0;
                     switch (int.Parse(item))
                     {
@@ -87,7 +105,14 @@ namespace MSXUtilities
 
                         case 2:
                             // Diamond
-                            intValueConverted = 0;
+                            //intValueConverted = 0;
+                            arrayBgObjectsScreens[currentScreen] += 
+                                String.Format("\tdb      {0},      {1},     {2} * 2 * 8,      1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0" + Environment.NewLine,
+                                x,
+                                "DIAMOND_FIRST_TILE",
+                                y
+                                );
+
                             break;
 
                         case 3:
@@ -104,10 +129,30 @@ namespace MSXUtilities
                             break;
                     }
                     outputLine.Add(intValueConverted);
+
+                    x++;
                 }
                 
                 output.Add(outputLine);
+
+                y++;
             }
+
+            // Save BgObjects file
+            int index = 0;
+            foreach (var item in arrayBgObjectsScreens)
+            {
+                bgObjectsText += "; Screen #" + index + Environment.NewLine;
+                bgObjectsText += item;
+                bgObjectsText += String.Format(
+                    "\tds     256 - ($ - ({0} + {1})), 0                 ; fill with 0s until end of block" + Environment.NewLine + Environment.NewLine,
+                    startLabel,
+                    index * 256
+                    );
+                index++;
+            }
+            bgObjectsText += "BgObjectsInitialState_TestLevel_2_End:" + Environment.NewLine;
+            File.WriteAllText("BgObjects.s", bgObjectsText);
 
             return output;
         }
