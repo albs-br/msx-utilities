f229a68ccdec71463391e92a3e2299b1d0f2c60a
Andre Baptista <albs_br@yahoo.com.br>
2021-12-25 17:31:22 -0300

    v.0.44.5

--- MSXUtilities/Program.cs
+++ MSXUtilities/Program.cs
@@ -87,13 +87,15 @@ namespace MSXUtilities
             //ConvertTinySpriteBkpToSc5Format(filename);
             //filename = @"Msxmas21\window 4.txt";
             //ConvertTinySpriteBkpToSc5Format(filename);
-            var filename = @"Msxmas21\small gift 1.txt";
-            ConvertTinySpriteBkpToSc5Format(filename);
-            filename = @"Msxmas21\small gift 2.txt";
-            ConvertTinySpriteBkpToSc5Format(filename);
-            filename = @"Msxmas21\small gift 3.txt";
-            ConvertTinySpriteBkpToSc5Format(filename);
-            filename = @"Msxmas21\small gift 4.txt";
+            //var filename = @"Msxmas21\small gift 1.txt";
+            //ConvertTinySpriteBkpToSc5Format(filename);
+            //filename = @"Msxmas21\small gift 2.txt";
+            //ConvertTinySpriteBkpToSc5Format(filename);
+            //filename = @"Msxmas21\small gift 3.txt";
+            //ConvertTinySpriteBkpToSc5Format(filename);
+            //filename = @"Msxmas21\small gift 4.txt";
+            //ConvertTinySpriteBkpToSc5Format(filename);
+            var filename = @"Msxmas21\bricks1.txt";
             ConvertTinySpriteBkpToSc5Format(filename);
 
 
