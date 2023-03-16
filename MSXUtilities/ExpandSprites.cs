3a2aee212d9c40ab3993169d36108302b2da4985
ANDRE BAPTISTA <andrebaptista@petrobras.com.br>
2023-03-16 15:42:26 -0300

    v.0.59.0

---
+++ MSXUtilities/ExpandSprites.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSXUtilities
+{
+    public static class ExpandSprites_Class
+    {
+		public static void ExpandSprites()
+		{
+
+			// ; -------------------------char #0 -------------------------
+			// ; ------pattern 0
+            string input = @"
+					db	00000000 b
+					db	00000000 b
+					db	00111111 b
+					db	01111111 b
+					db	01111111 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111111 b
+					db	01111111 b
+					db	00111111 b
+					db	00000000 b
+					db	00000000 b
+					db	00000000 b
+					db	11111000 b
+					db	11111100 b
+					db	11111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	11111100 b
+					db	11111100 b
+					db	11111000 b
+					db	00000000 b
+				 ";
+		}
+    }
+}
