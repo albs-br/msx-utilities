c1629d7299a4086911ef32dd7151523aaad3349b
ANDRE BAPTISTA <andrebaptista@petrobras.com.br>
2023-03-17 11:19:53 -0300

    v.0.59.3

--- MSXUtilities/ExpandSprites.cs
+++ MSXUtilities/ExpandSprites.cs
@@ -6,45 +6,63 @@ namespace MSXUtilities
 {
     public static class ExpandSprites_Class
     {
-		public static void ExpandSprites()
+		/// <summary>
+		/// Expand a 16x16 MSX 2 sprite to a factor, e.g. factor 2 will return
+		/// a 32x32 sprite, factor 3 a 48x48, and so on
+		/// </summary>
+		/// <param name="factor">2, 3, 4, 5</param>
+		/// <exception cref="Exception"></exception>
+		public static IList<string> ExpandSprites(string input, int factor)
 		{
+			
+			// convert input to list of strings with only the bits
+			var tempArray = input.Split(Environment.NewLine);
+			var listInput = new List<string>();
+            for (int i = 0; i < tempArray.Length; i++)
+			{
+				var tempItem = tempArray[i].Replace("db", "").Replace("b", "").Trim();
 
-			// ; -------------------------char #0 -------------------------
-			// ; ------pattern 0
-            string input = @"
-					db	00000000 b
-					db	00000000 b
-					db	00111111 b
-					db	01111111 b
-					db	01111111 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111111 b
-					db	01111111 b
-					db	00111111 b
-					db	00000000 b
-					db	00000000 b
-					db	00000000 b
-					db	11111000 b
-					db	11111100 b
-					db	11111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	01111100 b
-					db	11111100 b
-					db	11111100 b
-					db	11111000 b
-					db	00000000 b
-				 ";
-		}
+				if (tempItem != "")
+				{
+                    if (tempItem.Length != 8) throw new Exception("Lines on input must be 8 bits long");
+
+                    listInput.Add(tempItem); 
+				}
+			}
+
+			if (listInput.Count != 32) throw new Exception("Number of lines on input must be 32");
+
+
+            // convert list from 32 lines of 8 bits to 16 lines of 16 bits
+            var listInput_16x16 = new List<string>();
+            for (int i = 0; i < 16; i++)
+			{
+				listInput_16x16.Add(listInput[i] + listInput[i+16]);
+			}
+
+
+
+            // do the scaling
+			var listaOutput = new List<string>();
+			for (int i = 0; i < 16; i++)
+			{
+
+				var tempOutputLine = "";
+				for (int j = 0; j < 16; j++)
+				{
+					for (int k = 0; k < factor; k++)
+					{
+						tempOutputLine += listInput_16x16[i][j];
+					}
+				}
+
+				for (int k = 0; k < factor; k++)
+				{
+					listaOutput.Add(tempOutputLine);
+				}
+			}
+
+			return listaOutput;
+        }
     }
 }
---
+++ MsxUtilities.Test/ExpandSpritesTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSXUtilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MsxUtilities.Test
+{
+    [TestClass]
+    public class ExpandSpritesTest
+    {
+        // ; -------------------------char #0 -------------------------
+        // ; ------pattern 0
+        string input = @"
+					db	00000000 b
+					db	00000000 b
+					db	00111111 b
+					db	01111111 b
+					db	01111111 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111111 b
+					db	01111111 b
+					db	00111111 b
+					db	00000000 b
+					db	00000000 b
+					db	00000000 b
+					db	11111000 b
+					db	11111100 b
+					db	11111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	01111100 b
+					db	11111100 b
+					db	11111100 b
+					db	11111000 b
+					db	00000000 b
+				 ";
+
+        [TestMethod]
+        public void Test_ExpandSprite_Factor_2()
+        {
+            // Arrange
+            int factor = 2;
+
+            // Act
+            var output = ExpandSprites_Class.ExpandSprites(input, factor);
+
+            // Assert
+            Assert.AreEqual(factor * 16, output.Count);			// number of lines on output
+            Assert.AreEqual(factor * 16, output[0].Length);		// number of columns on line
+
+        }
+
+        [TestMethod]
+        public void Test_ExpandSprite_Factor_3()
+        {
+            // Arrange
+            int factor = 3;
+
+            // Act
+            var output = ExpandSprites_Class.ExpandSprites(input, factor);
+
+            // Assert
+            Assert.AreEqual(factor * 16, output.Count);			// number of lines on output
+            Assert.AreEqual(factor * 16, output[0].Length);		// number of columns on line
+        }
+
+        [TestMethod]
+        public void Test_ExpandSprite_Factor_4()
+        {
+            // Arrange
+            int factor = 4;
+
+            // Act
+            var output = ExpandSprites_Class.ExpandSprites(input, factor);
+
+            // Assert
+            Assert.AreEqual(factor * 16, output.Count);			// number of lines on output
+            Assert.AreEqual(factor * 16, output[0].Length);		// number of columns on line
+        }
+    }
+}
