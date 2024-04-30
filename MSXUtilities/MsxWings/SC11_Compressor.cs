521e52c658f1a485df0ba3cd4236532963991430
ANDRE BAPTISTA <andrebaptista@petrobras.com.br>
2024-04-30 12:19:17 -0300

    v.0.66.7

--- MSXUtilities/MsxWings/SC11_Compressor.cs
+++ MSXUtilities/MsxWings/SC11_Compressor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,7 @@ namespace MSXUtilities.MsxWings
         ///    255, 0-254  two-byte index(255 values)
         /// Literals: 5 bytes
         ///     0, 4 bytes literal
+        /// Ratio aprox: 0.5 of original size
         /// </summary>
         public static void Method_1()
         {
@@ -107,6 +110,26 @@ namespace MSXUtilities.MsxWings
             Console.WriteLine("ratio: " + ratio);
         }
 
+        /// <summary>
+        /// 
+        /// 64kb(or 48kb) of VRAM used as sliding window for packing.
+        /// packing a new line(256 bytes) by searching blocks of repeated bytes(4 up to 127)
+        /// 
+        /// format:
+        /// 
+        /// size of block/size of literal:  1 byte
+        ///     bit 7: 1: literal; 0: repeat block
+        /// 
+        /// offset addr:			2 bytes(1 word)
+        /// 
+        /// 
+        /// 
+        /// ex:
+        /// 0x07, 1, 4, 190, 0, 0, 1, 19 : 	7 literal bytes(1 to 19)
+        /// 1000 1111 b, 0x409f :		repeat 15 bytes started at 0x409f
+        /// 
+        /// Ratio aprox: 0.44 of original size (line of 256 bytes packed to 113 bytes)
+        /// </summary>
         public static void Method_2()
         {
             // 16 pages of 16 kb each
@@ -118,7 +141,7 @@ namespace MSXUtilities.MsxWings
             int windowEnd = (16 * 1024 * 16) - 1;   // end of last page
             
 
-            //TODO: test just on line
+            // provisory; debug: testing just one line compression
 
             int inputCurrentPosition = windowStart - 256; // start of last line of page 11
             while (inputCurrentPosition < windowStart)
@@ -182,8 +205,8 @@ namespace MSXUtilities.MsxWings
 
                             // populate output, update vars
                             output.Add((byte)block.Length); // block header
-                            output.Add((byte)(i & 0x00ff)); // address low byte
-                            output.Add((byte)((i & 0xff00) >> 8)); // address hi byte
+                            output.Add((byte)(i & 0x000ff)); // address low byte
+                            output.Add((byte)((i & 0x0ff00) >> 8)); // address hi byte
                             inputCurrentPosition += blockSize;
                             found_1 = true;
                             found_2 = true;
@@ -214,5 +237,114 @@ namespace MSXUtilities.MsxWings
                 }
             }
         }
+
+        /// <summary>
+        /// Idem to Method 2, but trying the bigger block size on all input, before going to a smaller block size
+        /// Ratio aprox: 0.5 of original size (line of 256 bytes packed to 69 bytes, but missing 32 bytes, which would have to be literals)
+        /// </summary>
+        public static void Method_3()
+        {
+            // 16 pages of 16 kb each
+            byte[] input = File.ReadAllBytes(@"C:\Users\XDAD\source\repos\msx-wings\Graphics\Bitmaps\Level_1\level_1_all.sca");
+            //byte[] output = Array.Empty<byte>();
+            IList<byte> output = new List<byte>();
+
+            int windowStart = 16 * 1024 * 12;       // start of page 12
+            int windowEnd = (16 * 1024 * 16) - 1;   // end of last page
+
+
+            // debug: test just one line
+            int inputCurrentPosition = windowStart - 256; // start of last line of page 11
+            bool[] newLinePositions = new bool[256]; // array indicating positions already compressed
+            for (int i = 0; i < 256; i++)
+            {
+                newLinePositions[i] = false;
+            }
+            for (int blockSize = 32; blockSize >= 4; blockSize--)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Trying block size " + blockSize);
+
+                // try this block size on all positions of the input (new line)
+                for (int k = inputCurrentPosition, positionOnNewLine = 0; k < windowStart - blockSize; k++, positionOnNewLine++)
+                {
+                    byte[] block = new byte[blockSize];
+
+                    //populate block array
+                    bool positionsAlreadyResolved = false;
+                    for (int i = 0; i < blockSize; i++)
+                    {
+                        if (newLinePositions[positionOnNewLine + i]) positionsAlreadyResolved = true;
+                        block[i] = input[k + i];
+                    }
+
+                    if (!positionsAlreadyResolved)
+                    {
+                        for (int i = windowStart; i < windowEnd - blockSize; i++)
+                        {
+                            bool found = true;
+                            for (int j = 0; j < blockSize; j++)
+                            {
+                                if (block[j] != input[i + j])
+                                {
+                                    found = false;
+                                }
+                            }
+
+                            if (found)
+                            {
+                                Console.WriteLine("  Block size: " + blockSize);
+
+                                Console.WriteLine();
+                                Console.WriteLine("    Found: input position: " + i);
+                                Console.Write("      ");
+                                for (int j = 0; j < blockSize; j++)
+                                {
+                                    Console.Write(input[i + j] + ",");
+                                }
+
+                                Console.WriteLine();
+                                Console.WriteLine("    new line position absolute: " + k);
+                                Console.WriteLine("    new line position relative: " + positionOnNewLine);
+                                Console.Write("      ");
+                                for (int j = 0; j < blockSize; j++)
+                                {
+                                    Console.Write(block[j] + ",");
+                                }
+
+
+                                // set positions resolved to be ignored on next iterations
+                                for (int j = 0; j < blockSize; j++)
+                                {
+                                    newLinePositions[positionOnNewLine + j] = true;
+                                }
+
+                                // populate output, update vars
+                                output.Add((byte)block.Length); // block header
+                                output.Add((byte)(i & 0x000ff)); // address low byte
+                                output.Add((byte)((i & 0x0ff00) >> 8)); // address hi byte
+                                                                        //inputCurrentPosition += blockSize;
+                                                                        //found_1 = true;
+                                                                        //found_2 = true;
+
+                                Console.WriteLine();
+                                Console.WriteLine("    output current size: " + output.Count);
+
+                                //Console.ReadKey();
+
+                                break;
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+            // check if line is resolved
+            Console.WriteLine("solved positions: " + newLinePositions.Where(x => x == true).ToList().Count);
+
+        }
+
     }
 }
